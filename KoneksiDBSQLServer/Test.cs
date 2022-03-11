403f8a19d5e3271209ae5734f7efe906606505e0
Ardiansyah <aardi25@gmail.com>
2022-03-11 09:50:48 +0700

    Penambahan Class Test

---
+++ KoneksiDBSQLServer/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoneksiDBSQLServer
+{
+    class Test
+    {
+    }
+}
